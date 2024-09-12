c2f2f93515269e23c059e3aa0caaf11dc19b72b0
juneaukim <juneaudarim@gmail.com>
2024-09-12 14:31:55 +0900

    Add btn2 clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, 
-                             QPlainTextEdit)
+                             QPlainTextEdit, QHBoxLayout)
 
 from PyQt5.QtGui import QIcon 
 
@@ -17,10 +17,19 @@ class Calculator(QWidget) :
         self.btn1 = QPushButton('Message', self)
         self.btn1.clicked.connect(self.activateMessage)
 
+        self.btn2=QPushButton('Clear',self)
+        self.btn2.clicked.connect(self.clearMessage)
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1)
         vbox.addStretch(1)
         vbox.addWidget(self.btn1)
+        vbox.addLayout(hbox)
         vbox.addStretch(1)
 
         self.setLayout(vbox)
@@ -31,9 +40,13 @@ class Calculator(QWidget) :
         self.resize(256,256)
         self.show()
 
+
     def activateMessage(self) :
         #QMessageBox.information(self, "information", "Button clicked!")
         self.te1.appendPlainText("button clicked!")
+
+    def clearMessage(self) :
+        self.te1.clear()
 # random nonsense
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
